137f9d39dc1088ec02d207af09e520ba60820b4f
Rafael Noll <rafanoll2004@gmail.com>
2023-06-16 10:42:15 -0300

    Feature: Adding first button/link into the sideMenu

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 
 import * as S from './styles';
-
 import Logo from '../../assets/logo.svg';
+import BoardsIcon from '../../assets/board-icon.svg';
+
+import Button from '../Button';
 
 function SideMenu() {
   return (
     <S.Container>
       <S.Content>
         <S.LogoImage src={Logo as unknown as string} />
+
+        <S.ButtonsContainer>
+          <Button
+            asLink
+            href="#"
+            transparent
+            text="Boards"
+            icon={BoardsIcon as unknown as string}
+          />
+        </S.ButtonsContainer>
       </S.Content>
     </S.Container>
   );
--- src/components/SideMenu/styles.ts
+++ src/components/SideMenu/styles.ts
@@ -15,4 +15,11 @@ export const Content = styled.div`
 
 export const LogoImage = styled.img``;
 
-export const ButtonsContainer = styled.div``;
+export const ButtonsContainer = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    gap: ${theme.spacing.small};
+    margin-top: ${theme.spacing.large};
+  `}
+`;
