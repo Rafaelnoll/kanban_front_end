4a950f24af22d5f276d4280487bbb8394b04a89f
Rafael Noll <rafanoll2004@gmail.com>
2023-06-16 10:41:11 -0300

    Style(Button): Button style change when it is a link

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -4,22 +4,22 @@ interface ButtonProps {
   transparent?: boolean;
 }
 
+export const Text = styled.span``;
+
 export const Button = styled.button<ButtonProps>`
-  ${({ theme, transparent }) => css`
+  ${({ theme, transparent, as }) => css`
     color: ${theme.color.white};
     background-color: ${transparent ? 'transparent' : theme.color.primary};
-    padding: ${theme.spacing.medium} ${theme.spacing.big};
+    padding: ${theme.spacing.medium} ${as ? 0 : theme.spacing.big};
     border: none;
     border-radius: 5px;
     font-size: ${theme.size.medium};
     display: flex;
     align-items: center;
-    justify-content: center;
-    gap: 8px;
+    justify-content: ${as ? 'none' : 'center'};
+    gap: ${as ? theme.spacing.medium : '8px'};
     cursor: pointer;
   `}
 `;
 
 export const Icon = styled.img``;
-
-export const Text = styled.span``;
