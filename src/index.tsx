b15c1046386b0ff438eb2fbd71a17276beb632e7
Rafael Noll <rafanoll2004@gmail.com>
2023-06-16 12:50:19 -0300

    Fix(react): Adding the new way to create a root in react(createRoot)

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import reactDom from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 
 import App from './App';
 
-reactDom.render(
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
   <ThemeProvider theme={theme}>
     <App />
   </ThemeProvider>,
-  document.getElementById('root'),
 );
