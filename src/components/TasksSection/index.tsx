947bc703a059454b4271aa18a9247f4669b0b004
Rafael Noll <rafanoll2004@gmail.com>
2023-06-20 10:26:32 -0300

    Feature: Adding TaskModal to component TasksSection

--- src/components/TasksSection/index.tsx
+++ src/components/TasksSection/index.tsx
@@ -1,86 +1,107 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import * as S from './styles';
 import Task from '../Task';
 import AddIcon from '../../assets/add-icon.svg';
+import TaskModal from '../TaskModal';
 
 function TasksSection() {
+  const [isTaskModalOpen, setIsTaskModalOpen] = useState(false);
+
+  function handleCancelTaskModal() {
+    setIsTaskModalOpen(false);
+  }
+
+  function handleOpenTaskModal() {
+    setIsTaskModalOpen(true);
+  }
+
   return (
-    <S.Container>
-      <S.TasksContainer>
-        <S.TasksContainerHeader>
-          <S.Label>A fazer</S.Label>
-          <AddIcon />
-        </S.TasksContainerHeader>
+    <>
+      {isTaskModalOpen && (
+        <TaskModal
+          title="Criar Tarefa"
+          onCancel={handleCancelTaskModal}
+          onSave={() => console.log('save')}
+        />
+      )}
 
-        <S.TasksList>
-          <Task
-            title="#Bora codar um kanban"
-            description="        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed
+      <S.Container>
+        <S.TasksContainer>
+          <S.TasksContainerHeader>
+            <S.Label>A fazer</S.Label>
+            <AddIcon onClick={handleOpenTaskModal} />
+          </S.TasksContainerHeader>
+
+          <S.TasksList>
+            <Task
+              title="#Bora codar um kanban"
+              description="        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed
         tempor mauris. Quisque ullamcorper aliquet ex, in mattis mauris ornare
         in. Phasellus vehicula finibus augue, sit amet dignissim lorem cursus
         euismod."
-            category="Programação"
-          />
-          <Task
-            title="#Bora codar um kanban"
-            description="        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed
-        tempor mauris. Quisque ullamcorper aliquet ex, in mattis mauris ornare
-        in. Phasellus vehicula finibus augue, sit amet dignissim lorem cursus
-        euismod."
-            category="Programação"
-          />
-        </S.TasksList>
-      </S.TasksContainer>
+              category="Programação"
+            />
+            <Task
+              title="#Bora codar um kanban"
+              description="        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed
+            tempor mauris. Quisque ullamcorper aliquet ex, in mattis mauris ornare
+            in. Phasellus vehicula finibus augue, sit amet dignissim lorem cursus
+            euismod."
+              category="Programação"
+            />
+          </S.TasksList>
+        </S.TasksContainer>
 
-      <S.TasksContainer>
-        <S.TasksContainerHeader>
-          <S.Label>Fazendo</S.Label>
-          <AddIcon />
-        </S.TasksContainerHeader>
-        <S.TasksList>
-          <Task
-            title="Estudar React"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed
-        tempor mauris."
-            category="Estudo"
-          />
-          <Task
-            title="#Bora codar um kanban"
-            description="        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed
-        tempor mauris. Quisque ullamcorper aliquet ex, in mattis mauris ornare
-        in. Phasellus vehicula finibus augue, sit amet dignissim lorem cursus
-        euismod."
-            category="Programação"
-          />
-        </S.TasksList>
-      </S.TasksContainer>
+        <S.TasksContainer>
+          <S.TasksContainerHeader>
+            <S.Label>Fazendo</S.Label>
+            <AddIcon />
+          </S.TasksContainerHeader>
+          <S.TasksList>
+            <Task
+              title="Estudar React"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed
+            tempor mauris."
+              category="Estudo"
+            />
+            <Task
+              title="#Bora codar um kanban"
+              description="        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed
+            tempor mauris. Quisque ullamcorper aliquet ex, in mattis mauris ornare
+            in. Phasellus vehicula finibus augue, sit amet dignissim lorem cursus
+            euismod."
+              category="Programação"
+            />
+          </S.TasksList>
+        </S.TasksContainer>
 
-      <S.TasksContainer>
-        <S.TasksContainerHeader>
-          <S.Label>Feito</S.Label>
-          <AddIcon />
-        </S.TasksContainerHeader>
+        <S.TasksContainer>
+          <S.TasksContainerHeader>
+            <S.Label>Feito</S.Label>
+            <AddIcon />
+          </S.TasksContainerHeader>
 
-        <S.TasksList>
-          <Task
-            title="Escovar os dentes"
-            description="Quisque ullamcorper aliquet ex, in mattis mauris ornare
-        in. Phasellus vehicula finibus augue, sit amet dignissim lorem cursus
+          <S.TasksList>
+            <Task
+              title="Escovar os dentes"
+              description="Quisque ullamcorper aliquet ex, in mattis mauris ornare
+            in. Phasellus vehicula finibus augue, sit amet dignissim lorem cursus
         euismod."
-            category="Saúde"
-          />
-          <Task
-            title="#Bora codar um kanban"
-            description="        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed
-        tempor mauris. Quisque ullamcorper aliquet ex, in mattis mauris ornare
-        in. Phasellus vehicula finibus augue, sit amet dignissim lorem cursus
-        euismod."
-            category="Programação"
-          />
-        </S.TasksList>
-      </S.TasksContainer>
-    </S.Container>
+              category="Saúde"
+            />
+            <Task
+              title="#Bora codar um kanban"
+              description="        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed
+            tempor mauris. Quisque ullamcorper aliquet ex, in mattis mauris ornare
+            in. Phasellus vehicula finibus augue, sit amet dignissim lorem cursus
+            euismod."
+              category="Programação"
+            />
+          </S.TasksList>
+        </S.TasksContainer>
+      </S.Container>
+    </>
   );
 }
 
