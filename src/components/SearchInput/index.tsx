2cdc2526b2069c224afe83586a37fa660b015e52
Rafael Noll <rafanoll2004@gmail.com>
2023-06-29 15:00:13 -0300

    Feature: Adding prop onChange on component SearchInput to control the events of the SearchInput

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,13 +1,16 @@
-import React, { useRef } from 'react';
+import React, { ChangeEvent, useRef } from 'react';
 
 import * as S from './styles';
 import SearchIcon from '../../assets/search-icon.svg';
 
 interface InputProp {
   placeholder?: string;
+  name: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  value: string;
 }
 
-function SearchInput({ placeholder }: InputProp) {
+function SearchInput({ placeholder, name, value, onChange }: InputProp) {
   const inputRef = useRef<HTMLInputElement | null>(null);
 
   function handleClickOnInput() {
@@ -19,7 +22,13 @@ function SearchInput({ placeholder }: InputProp) {
   return (
     <S.Container onClick={handleClickOnInput}>
       <SearchIcon />
-      <S.Input placeholder={placeholder} ref={inputRef} />
+      <S.Input
+        placeholder={placeholder}
+        ref={inputRef}
+        name={name}
+        value={value}
+        onChange={(event) => onChange(event)}
+      />
     </S.Container>
   );
 }
