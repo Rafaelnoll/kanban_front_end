f598abc003eb2c0f4ae073873ff77649f70889ab
Rafael Noll <rafanoll2004@gmail.com>
2023-06-30 16:46:01 -0300

    Feature: Adding props onUpdate and onDelete in TableCategory to controll the update and delete event

--- src/components/TableCategory/index.tsx
+++ src/components/TableCategory/index.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import * as S from './styles';
 import EditIcon from '../../assets/edit-icon.svg';
 import DeleteIcon from '../../assets/trash-icon.svg';
 import { Category } from '../../interfaces/Category';
+import CategoryModal from '../CategoryModal';
+import { FormCategoriesInputs } from '../../interfaces/FormInputs';
 
-function TableCategory({ name, tasks_count }: Category) {
+interface TableCategoryProps extends Category {
+  onDelete: (categoryId: string) => void;
+  onUpdate: (props: FormCategoriesInputs, id: string) => void;
+}
+
+function TableCategory({
+  name,
+  tasks_count,
+  id,
+  onDelete,
+  onUpdate,
+}: TableCategoryProps) {
+  const [isModalEditOpen, setIsModalEditOpen] = useState(false);
+
+  function handleModalClose() {
+    setIsModalEditOpen(false);
+  }
+
+  function handleModalOpen() {
+    setIsModalEditOpen(true);
+  }
+
+  function handleUpdateCategory(data: FormCategoriesInputs) {
+    onUpdate(data, id);
+    handleModalClose();
+  }
+
+  function handleDeleteCategory() {
+    onDelete(id);
+    handleModalClose();
+  }
+
   return (
-    <S.TableRow>
-      <td>{name}</td>
-      <td>{tasks_count}</td>
-      <td>
-        <S.ActionButtonContainer>
-          <S.ActionButton>
-            <EditIcon />
-          </S.ActionButton>
-          <S.ActionButton>
-            <DeleteIcon />
-          </S.ActionButton>
-        </S.ActionButtonContainer>
-      </td>
-    </S.TableRow>
+    <>
+      <S.TableRow>
+        <td>{name}</td>
+        <td>{tasks_count}</td>
+        <td>
+          <S.ActionButtonContainer>
+            <S.ActionButton onClick={handleModalOpen}>
+              <EditIcon />
+            </S.ActionButton>
+            <S.ActionButton onClick={handleDeleteCategory}>
+              <DeleteIcon />
+            </S.ActionButton>
+          </S.ActionButtonContainer>
+        </td>
+        {isModalEditOpen && (
+          <td style={{ width: '0%', padding: '0' }}>
+            <CategoryModal
+              onCancel={handleModalClose}
+              title="Modificar Categoria"
+              initialData={{
+                name,
+              }}
+              onSubmitEvent={handleUpdateCategory}
+            />
+          </td>
+        )}
+      </S.TableRow>
+    </>
   );
 }
 
