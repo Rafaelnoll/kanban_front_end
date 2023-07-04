39de22ab4a0b25bf3f23dfcf81ed4569673e232f
Rafael Noll <rafanoll2004@gmail.com>
2023-07-04 09:30:01 -0300

    Feature: Adding login page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import GlobalStyles from './styles/GlobalStyles';
 import KanbanPage from './pages/KanbanPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ManegeCategories from './pages/ManageCategories';
+import LoginPage from './pages/Login';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<KanbanPage />} />
           <Route path="/categories" element={<ManegeCategories />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </BrowserRouter>
       <ToastContainer position="bottom-center" />
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+
+import * as S from './styles';
+import KanbanImage from '../../assets/login-kanban.svg';
+import LogoIcon from '../../assets/logo.svg';
+import Input from '../../components/Input';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import Button from '../../components/Button';
+
+interface FormLoginInputs {
+  email: string;
+  password: string;
+}
+
+const schema = yup.object({
+  email: yup.string().required('Email é obrigatório.').email('E-mail inválido'),
+  password: yup.string().required('Senha é obrigatório.'),
+});
+
+function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormLoginInputs>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<FormLoginInputs> = ({
+    email,
+    password,
+  }: FormLoginInputs) => {
+    console.log(email, password);
+  };
+
+  return (
+    <S.Container>
+      <S.ImageContainer>
+        <KanbanImage />
+      </S.ImageContainer>
+
+      <S.FormContainer>
+        <S.FormHeader>
+          <LogoIcon />
+          <S.Text>Kanban</S.Text>
+        </S.FormHeader>
+
+        <S.Title>Bem-Vindo novamente</S.Title>
+
+        <S.Form onSubmit={handleSubmit(onSubmit)}>
+          <S.InputContainer>
+            <S.Label>E-mail</S.Label>
+            <Input
+              register={register}
+              name="email"
+              placeholder="Digite o seu e-mail"
+              type="email"
+            />
+            {errors.email && (
+              <S.ErrorMessage>{errors.email.message}</S.ErrorMessage>
+            )}
+          </S.InputContainer>
+
+          <S.InputContainer>
+            <S.Label>Senha</S.Label>
+            <Input
+              register={register}
+              name="password"
+              placeholder="Digite a sua senha"
+              type="password"
+            />
+            {errors.password && (
+              <S.ErrorMessage>{errors.password.message}</S.ErrorMessage>
+            )}
+          </S.InputContainer>
+
+          <S.FormLink to="#">Esqueceu a senha?</S.FormLink>
+
+          <Button text="Sign In" type="submit" />
+
+          <S.Separator />
+
+          <S.SmallText>
+            Não tem uma conta? <S.FormLink to="#">Registre-se agora</S.FormLink>
+          </S.SmallText>
+        </S.Form>
+      </S.FormContainer>
+    </S.Container>
+  );
+}
+
+export default LoginPage;
