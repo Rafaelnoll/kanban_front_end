20819f45bd47d3b9bf711d505b0c7af214507fec
Rafael Noll <rafanoll2004@gmail.com>
2023-07-19 14:15:01 -0300

    Style: Modifying box shadow of List in component ButtonFilter

--- src/components/ButtonFilter/styles.ts
+++ src/components/ButtonFilter/styles.ts
@@ -36,7 +36,8 @@ export const List = styled.div`
   ${({ theme }) => css`
     position: absolute;
     background-color: ${theme.color.white};
-    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+    box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+      rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
     z-index: 998;
     left: 0;
     border-radius: 8px;
