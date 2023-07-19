b7ec991a5bb6ecea31df422e87a5864f89730458
Rafael Noll <rafanoll2004@gmail.com>
2023-07-19 15:23:54 -0300

    Refactor: Adding the template SideForm in pages of register, login and forgot-password

--- src/pages/ForgotPasswordPage/index.tsx
+++ src/pages/ForgotPasswordPage/index.tsx
@@ -1,82 +1,13 @@
 import React from 'react';
 
-import * as S from './styles';
-import KanbanImage from '../../assets/login-kanban.svg';
-import LogoIcon from '../../assets/logo.svg';
-import Input from '../../components/Input';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
-import Button from '../../components/Button';
-
-interface FormForgotPasswordInputs {
-  email: string;
-}
-
-const schema = yup.object({
-  email: yup.string().required('Email é obrigatório.').email('E-mail inválido'),
-});
+import { SideFormPage } from '../../templates/SideFormPage';
+import { FormForgotPassword } from '../../components/FormForgotPassword';
 
 function ForgotPasswordPage() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormForgotPasswordInputs>({
-    defaultValues: {
-      email: '',
-    },
-    resolver: yupResolver(schema),
-  });
-
-  const onSubmit: SubmitHandler<FormForgotPasswordInputs> = ({
-    email,
-  }: FormForgotPasswordInputs) => {
-    console.log(email);
-  };
-
   return (
-    <S.Container>
-      <S.ImageContainer>
-        <KanbanImage />
-      </S.ImageContainer>
-
-      <S.FormContainer>
-        <S.FormHeader>
-          <LogoIcon />
-          <S.Text>Kanban</S.Text>
-        </S.FormHeader>
-
-        <S.TextContainer>
-          <S.Title>Esqueceu a senha?</S.Title>
-          <S.Description>
-            Não se preocupe! Isso acontece. Por favor, informe o seu endereço de
-            de e-mail conectado à sua conta para que eu possa ajudar você.
-          </S.Description>
-        </S.TextContainer>
-
-        <S.Form onSubmit={handleSubmit(onSubmit)}>
-          <S.InputContainer>
-            <S.Label>E-mail</S.Label>
-            <Input
-              register={register}
-              name="email"
-              placeholder="Digite o seu e-mail"
-              type="email"
-            />
-            {errors.email && (
-              <S.ErrorMessage>{errors.email.message}</S.ErrorMessage>
-            )}
-          </S.InputContainer>
-
-          <Button text="Enviar código" type="submit" />
-
-          <S.SmallText>
-            Lembra da senha? <S.FormLink to="/login">Login</S.FormLink>
-          </S.SmallText>
-        </S.Form>
-      </S.FormContainer>
-    </S.Container>
+    <SideFormPage title="Esqueceu a senha?">
+      <FormForgotPassword />
+    </SideFormPage>
   );
 }
 
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,115 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import * as S from './styles';
-import KanbanImage from '../../assets/login-kanban.svg';
-import LogoIcon from '../../assets/logo.svg';
-import Input from '../../components/Input';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
-import Button from '../../components/Button';
-import useAuthentication from '../../hooks/useAuthentication';
-import UserController from '../../controllers/UserController';
-import { useNavigate } from 'react-router-dom';
-
-interface FormLoginInputs {
-  email: string;
-  password: string;
-}
-
-const schema = yup.object({
-  email: yup.string().required('Email é obrigatório.').email('E-mail inválido'),
-  password: yup.string().required('Senha é obrigatório.'),
-});
+import { SideFormPage } from '../../templates/SideFormPage';
+import { FormLogin } from '../../components/FormLogin';
 
 function LoginPage() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormLoginInputs>({
-    defaultValues: {
-      email: '',
-      password: '',
-    },
-    resolver: yupResolver(schema),
-  });
-
-  const { user, handleLogin } = useAuthentication();
-  const navigate = useNavigate();
-
-  const onSubmit: SubmitHandler<FormLoginInputs> = async ({
-    email,
-    password,
-  }: FormLoginInputs) => {
-    const authenticationData = await UserController.getTokenAuthentication({
-      email,
-      password,
-    });
-
-    handleLogin(authenticationData);
-  };
-
-  useEffect(() => {
-    if (user) {
-      navigate('/dashboard');
-    }
-  });
-
   return (
-    <S.Container>
-      <S.ImageContainer>
-        <KanbanImage />
-      </S.ImageContainer>
-
-      <S.FormContainer>
-        <S.FormHeader>
-          <LogoIcon />
-          <S.Text>Kanban</S.Text>
-        </S.FormHeader>
-
-        <S.Title>Bem-Vindo novamente</S.Title>
-
-        <S.Form onSubmit={handleSubmit(onSubmit)}>
-          <S.InputContainer>
-            <S.Label>E-mail</S.Label>
-            <Input
-              register={register}
-              name="email"
-              placeholder="Digite o seu e-mail"
-              type="email"
-            />
-            {errors.email && (
-              <S.ErrorMessage>{errors.email.message}</S.ErrorMessage>
-            )}
-          </S.InputContainer>
-
-          <S.InputContainer>
-            <S.Label>Senha</S.Label>
-            <Input
-              register={register}
-              name="password"
-              placeholder="Digite a sua senha"
-              type="password"
-            />
-            {errors.password && (
-              <S.ErrorMessage>{errors.password.message}</S.ErrorMessage>
-            )}
-          </S.InputContainer>
-
-          <S.FormLink to="/forgot-password">Esqueceu a senha?</S.FormLink>
-
-          <Button text="Logar" type="submit" />
-
-          <S.Separator />
-
-          <S.SmallText>
-            Não tem uma conta?{' '}
-            <S.FormLink to="/register">Registre-se agora</S.FormLink>
-          </S.SmallText>
-        </S.Form>
-      </S.FormContainer>
-    </S.Container>
+    <SideFormPage title="Bem-Vindo novamente!">
+      <FormLogin />
+    </SideFormPage>
   );
 }
 
