3c1c1a772a22aef663a2655b28db8eb04ead4b57
Rafael Noll <rafanoll2004@gmail.com>
2023-09-19 14:18:05 -0300

    Refactor: Addinf custom hook in component CategoriesSection

--- src/components/Categories/CategoriesSection/index.tsx
+++ src/components/Categories/CategoriesSection/index.tsx
@@ -1,112 +1,22 @@
-import React, { ChangeEvent, useEffect, useState } from 'react';
+import React from 'react';
 
 import * as S from './styles';
 import SearchInput from '../../SearchInput';
-import { Category } from '../../../interfaces/Category';
 import CategoryModal from '../CategoryModal';
-import { FormCategoriesInputs } from '../../../interfaces/FormInputs';
 
-import CategoryController from '../../../controllers/CategoryController';
 import AddIcon from '../../../assets/add-icon.svg';
-import TableCategory from '../TableRow';
+import useCategoriesSection from './useCategoriesSection';
 
 function CategoriesSection() {
-  const [searchValue, setSearchValue] = useState('');
-  const [categories, setCategories] = useState<Category[]>([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  function handleChangeSearchValue(event: ChangeEvent<HTMLInputElement>) {
-    setSearchValue(event.target.value);
-  }
-
-  function handleOpenModal() {
-    setIsModalOpen(true);
-  }
-
-  function handleCancelModal() {
-    setIsModalOpen(false);
-  }
-
-  async function createCategory({ name }: FormCategoriesInputs) {
-    const categoryCreated = await CategoryController.store({ name });
-
-    if (categoryCreated) {
-      setCategories((prevstate) => {
-        prevstate.push({
-          ...categoryCreated,
-          tasks_count: '0',
-        });
-        return prevstate;
-      });
-      setIsModalOpen(false);
-    }
-  }
-
-  async function uptadeCategory({ name }: FormCategoriesInputs, id: string) {
-    const updatedCategory = await CategoryController.update({ name }, id);
-    if (updatedCategory) {
-      setCategories((prevState) =>
-        prevState.map((category) => {
-          if (category.id === id) {
-            return updatedCategory;
-          }
-
-          return category;
-        }),
-      );
-    }
-  }
-
-  async function deleteCategory(id: string) {
-    await CategoryController.delete(id);
-
-    setCategories((prevState) =>
-      prevState.filter((category) => category.id !== id),
-    );
-  }
-
-  function renderCategories() {
-    const filtredCategories = categories.filter((category) =>
-      category.name.toLowerCase().includes(searchValue.toLowerCase()),
-    );
-
-    if (searchValue.length >= 1) {
-      return filtredCategories.map((category) => (
-        <TableCategory
-          key={category.id}
-          name={category.name}
-          id={category.id}
-          tasks_count={category.tasks_count}
-          onUpdate={uptadeCategory}
-          onDelete={deleteCategory}
-        />
-      ));
-    }
-
-    return categories.map((category) => (
-      <TableCategory
-        key={category.id}
-        name={category.name}
-        id={category.id}
-        tasks_count={category.tasks_count}
-        onUpdate={uptadeCategory}
-        onDelete={deleteCategory}
-      />
-    ));
-  }
-
-  useEffect(() => {
-    async function loadCategories() {
-      try {
-        const categories = await CategoryController.index();
-        setCategories(categories);
-      } catch (error) {
-        setCategories([]);
-      }
-    }
-
-    loadCategories();
-  }, []);
+  const {
+    createCategory,
+    handleCancelModal,
+    handleChangeSearchValue,
+    handleOpenModal,
+    isModalOpen,
+    renderCategories,
+    searchValue,
+  } = useCategoriesSection();
 
   return (
     <>
